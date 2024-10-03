fe6ebb99f0ac91b6e99047814ba1cd429971ac04
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-03 05:18:32 -0300

    3. Serializar no formato XML

--- 01_SerializarXML/Program.cs
+++ 01_SerializarXML/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using _00_Biblioteca;
+using System.Xml.Serialization;
+
+namespace _01_SerializarXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Nome = "José Costa Silva", CPF = "222.222.222-22", Email = "jose@gmail.com"};
+        
+            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+            //XmlSerializer serializador = new XmlSerializer(usuario.GetType());
+
+            StreamWriter stream = new StreamWriter(@"C:\MyCode\Udemy\AdvancedC#\Advanced C#\01_SerializarXML.xml");
+        
+            serializador.Serialize(stream, usuario);
+        }
+    }
+}
