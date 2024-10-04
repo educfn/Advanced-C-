6efb4100d97b26c53929cc51cbe9459f53486204
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-03 21:45:20 -0300

    5. Serializar no formato JSON

---
+++ 03_SerializarJSON/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using _00_Biblioteca;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace _01_SerializarXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Nome = "Maria Costa Silva", CPF = "333.333.333-33", Email = "maria.costa@gmail.com" };
+
+            string stringObjetoSerializado = JsonConvert.SerializeObject(usuario);
+
+            StreamWriter sw = new StreamWriter(@"C:\MyCode\Udemy\AdvancedC#\Advanced C#\03_SerializarXML.xml");
+            sw.WriteLine(stringObjetoSerializado);
+            sw.Close();
+
+        }
+    }
+}
