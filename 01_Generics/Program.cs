d7a33149965fb161f9989feb1d6c8e3bdb2293f2
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-07 20:52:13 -0300

    8. Generics - Parte 1

---
+++ 01_Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_Generics.Modelo;
+
+namespace _01_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro() { Marca = "FIAT", Modelo="UNO"};
+            Casa casa = new Casa() { Cidade="Brasília", Endereco="QSQ 400"};
+            Usuario usuario = new Usuario() { Nome="Maria", Email="maria@gmail.com", Senha="123456"};
+
+            Serializador.Serializar(carro);
+            Serializador.Serializar(casa);
+            Serializador.Serializar(usuario);
+
+            Carro carro2 = Serializador.Deserializar();
+            Carro casa2 = Serializador.Deserializar();
+            Carro usuario2 = Serializador.Deserializar();
+        }
+    }
+}
---
+++ 01_Generics/Serializador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Generics
+{
+    public class Serializador
+    {
+        public static void Serializar(obj)
+        {
+
+        }
+        public static (Classe) Deserializar()
+        {
+
+        }
+    }
+}
