37d89210fc887bbab0936d0dc05a15433a9275e6
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-09 08:24:59 -0300

    Palavras Chave - Var e Dynamic

---
+++ 02_VarDinamic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object t1 = "";
+            object t2 = 123456;
+            t2 = "";
+
+            var v1 = "";
+            var v2 = 123456;
+            //var v3;
+            //ERRO: var v3;
+            //ERRO: v2 = "";
+
+            dynamic d1 = new Usuario { Nome="Maria"};
+
+            Console.WriteLine(d1.Nome);
+            //ERRO: Console.WriteLine(d1.Senha);
+
+            d1 = "";
+
+            Console.ReadKey();
+        }
+
+        class Usuario
+        {
+            public string Nome { get; set; }
+        }
+    }
+}
