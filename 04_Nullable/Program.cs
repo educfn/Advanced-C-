76ab6b80daa9ee2514daef835b6c86a8a7ef04d1
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-11 08:39:01 -0300

    13. Nullable

---
+++ 04_Nullable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Struct - Por Valor
+            int i = null;
+
+            //Class - Por referência
+            //MySQ - Oracle - PostGreeSQL ... No SQL - Informações Opcionais
+            Nullable<int> idade = null;
+            int? idade2 = null;
+            //ERRO: int Idade3 = null;
+        }
+    }
+}
