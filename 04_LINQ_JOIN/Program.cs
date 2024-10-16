86d6f59518f9b920fa8c088eef06b9e78ed88c7a
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-16 08:28:03 -0300

    19. LINQ - Join - Parte 2

--- 04_LINQ_JOIN/Program.cs
+++ 04_LINQ_JOIN/Program.cs
@@ -10,14 +10,24 @@ namespace _04_LINQ_JOIN
     {
         static void Main(string[] args)
         {
-            var autor1 = new Autor() { Id = 1, Nome = "Leonardo" };
-            var autor2 = new Autor() { Id = 2, Nome = "Maria Maria" };
-            var autor3 = new Autor() { Id = 3, Nome = "Joseph" };
+            List<Livro> listaLivro = new List<Livro>();
+            listaLivro.Add(new Livro() {Id = 1, AutorId = 2, Titulo = "Amor amado" });
+            listaLivro.Add(new Livro() {Id = 2, AutorId = 2, Titulo = "Bem amado" });
+            listaLivro.Add(new Livro() {Id = 3, AutorId = 3, Titulo = "Um espião em Washigton" });
+            listaLivro.Add(new Livro() {Id = 4, AutorId = 1, Titulo = "A Vida na terra" });
 
-            var livro1 = new Livro() {Id = 1, AutorId = 2, Titulo = "Amor amado" };
-            var livro2 = new Livro() {Id = 2, AutorId = 2, Titulo = "Bem amado" };
-            var livro3 = new Livro() {Id = 3, AutorId = 3, Titulo = "Um espião em Washigton" };
-            var livro4 = new Livro() {Id = 4, AutorId = 1, Titulo = "A Vida na terra" };
+            List<Autor> listaAutor = new List<Autor>();
+            listaAutor.Add(new Autor() { Id = 1, Nome = "Leonardo" });
+            listaAutor.Add(new Autor() { Id = 2, Nome = "Maria Maria" });
+            listaAutor.Add(new Autor() { Id = 3, Nome = "Joseph" });
+
+            var ListaJoin = from livros in listaLivro join autor in listaAutor on livros.AutorId equals autor.Id select new { livros, autor }; 
+
+            foreach (var item in ListaJoin)
+            {
+                Console.WriteLine("Livro: " + item.livros.Titulo + " - Autor: " + item.autor.Nome);
+            }
+            Console.ReadKey();
         }
     }
 }
