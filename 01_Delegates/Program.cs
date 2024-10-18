176033181668d15da5dc2bb9f4439a7cc7266dbb
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-17 21:23:03 -0300

    22. Delegate - Parte 1

---
+++ 01_Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Delegates
+{
+    class Program
+    {
+        delegate int Calcula(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Calcula calc = new Calcula(Soma);
+            int so = calc(10, 20);
+
+            Calcula calc1 = new Calcula(Subtrair);
+            int su = calc(35, 15);
+            /*
+            var so = Soma(10, 20);
+            var su = Subtrair(20 10);
+            */
+
+            Console.WriteLine("Soma: " + so);
+            Console.WriteLine("Sub: " + su);
+
+            Console.ReadKey();
+        }
+
+        static int Soma(int a, int b)
+        { 
+            return a + b; 
+        }
+
+        static int Subtrair(int a, int b)
+        { 
+            return a - b; 
+        }
+    }
+}
