f8fa65f0cccce41bba1aba4515353c0aaa61e2b6
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-18 20:42:48 -0300

    23. Delegate - Parte 2

---
+++ 02_Delegates/Lib/FotoFiltro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Delegates.Lib
+{
+    public class FotoFiltro
+    {
+        public void Colorir(Foto foto)
+        {
+            //Algoritmo q faz Colorimento
+            Console.Write("FotoFiltro > Colorir");
+        }
+
+        public void GerarThumb(Foto foto)
+        {
+            Console.Write("FotoFiltro > GerarThumb");
+        }
+
+        public void PretoBranco(Foto foto)
+        {
+            Console.Write("FotoFiltro > PretoBranco");
+        }
+
+        public void RedimensionarTamMedio(Foto foto)
+        {
+            Console.Write("FotoFiltro > RedimensionarTamMedio");
+        }
+    }
+}
