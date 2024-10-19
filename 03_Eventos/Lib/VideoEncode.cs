5266953194cf3a251715dbf4c4fc158b6e2f95b8
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-19 11:30:18 -0300

    Parar 25. Evento - Parte 1

---
+++ 03_Eventos/Lib/VideoEncode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Eventos.Lib
+{
+    public class VideoEncode
+    {
+        public delegate void VideoEncodedHandler(Video video);
+        public event VideoEncodedHandler Encoded;
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Convertendo o video...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Video convertido!");
+
+            Encoded(video);
+        }
+    }
+}
