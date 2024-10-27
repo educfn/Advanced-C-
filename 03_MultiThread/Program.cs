fff413d9bc7a5d9593d9fa2363ab031edc9c26dd
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-27 16:06:17 -0300

    35. Thread - Multi Thread

---
+++ 03_MultiThread/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace _03_MultiThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t1 = new Thread(ThreadRepeticao);
+                t1.Start();
+            }
+
+            Console.WriteLine("Programa finalizado!");
+            Console.ReadKey();
+        }
+        //IO - Tela(Monitor), Rede e Armazenamento.
+        static void ThreadRepeticao()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Num: " + i);
+            }
+        }
+    }
+}
