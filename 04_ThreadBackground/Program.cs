69d8cc60b149b0c4706c2cffe2d0703f07533b5c
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-27 16:06:33 -0300

    36. Thread - Em Background

---
+++ 04_ThreadBackground/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace _04_ThreadBackground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DataIni: " + DateTime.Now);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t1 = new Thread(ThreadRepeticao);
+                t1.IsBackground = true;
+                t1.Start();
+            }
+            Console.ReadKey();
+        }
+        //IO - Tela(Monitor), Rede e Armazenamento.
+        static void ThreadRepeticao()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Num: " + i);
+            }
+            Console.WriteLine("DateTime: " + DateTime.Now);
+        }
+    }
+}
