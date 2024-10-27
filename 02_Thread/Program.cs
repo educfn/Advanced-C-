ff82fe0afcfdc7ca7303f06cd93625311a286e50
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-27 15:39:17 -0300

    34. Thread - Primeiros passos

---
+++ 02_Thread/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(ThreadRepeticao);
+            t1.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Main: " + i);
+            }
+
+            Console.ReadKey();
+        }
+
+        //IO - Tela(Monitor), Rede e Armazenamento.
+        static void ThreadRepeticao()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Num: " + i);
+            }
+        }
+    }
+}
