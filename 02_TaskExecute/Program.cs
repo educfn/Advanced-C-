8223b777f20cf1aa4ce33cf550bfa980a58b29e3
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-10-30 08:16:38 -0300

    43. Task - Executar métodos

---
+++ 02_TaskExecute/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace _08_AutoManual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => Metodo01());
+            Task.Run(() => Metodo01());
+            Task.Factory.StartNew(Metodo01);
+            Console.ReadKey();
+        }
+
+        static void Metodo01()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Valor: " + i + " TaskID: " + Task.CurrentId);
+            }
+        }
+    }
+}
