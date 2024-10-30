8a252d391c8826e7c0e4ed28bacfe4d7da13cbb4
Eduardo C F Neto <eduardo@verion.ind.br>
2024-10-30 13:13:18 -0300

    46. Atributos

---
+++ 01_Atributo/MeuAtributo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Atributo
+{
+    class MeuAtributo : Attribute
+    {
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method |)]
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public MeuAtributo(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
---
+++ 01_Atributo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace _01_Atributo
+{
+    [MeuAtributo("Atributo Classe", Descricao = "Descrição do Atributo")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
