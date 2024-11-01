8ace478b51832615b4b2f23418cfe3a79bd74bcf
Eduardo C F Neto <eduardo@verion.ind.br>
2024-11-01 15:31:17 -0300

    48. Validacao - Data Annotation - Parte 2

---
+++ 03_ValidacaoCustomizada/MyValidacaoAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ValidacaoCustomizada
+{
+    class MyValidacaoAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (((string)value).Length == 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 03_ValidacaoCustomizada/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Threading;
+
+namespace _03_ValidacaoCustomizada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Nome = "José", Email = "jose", Senha = "1234" };
+            ValidationContext contexto = new ValidationContext(usuario);
+            List<ValidationResult> resultados = new List<ValidationResult>();
+
+            if (Validator.TryValidateObject(usuario, contexto, resultados, true) == false)
+            {
+                //Mensagens
+                foreach (var erro in resultados)
+                {
+                    Console.WriteLine(erro.ErrorMessage);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
