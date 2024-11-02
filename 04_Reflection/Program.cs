045b8dace8dbb2423e62f35f4b1715eb9499e679
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-11-02 10:45:29 -0300

    49. Reflection - Parte 1

---
+++ 04_Reflection/Program.cs
@@ -0,0 +1,29 @@
+﻿using _04_Reflection.Modelo;
+using System;
+using System.Net;
+using System.Threading;
+
+namespace _04_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario()
+            {
+                Nome = "José",
+                Email = "jose.costa@gmail.com",
+                Senha = "123456ab"
+            };
+            Log.GravarUsuario(usuario);
+
+            usuario.Nome = "José Costa";
+            Log.GravarUsuario(usuario);
+
+            Log.ApresentarLog();
+
+            Console.WriteLine("Log gravado!");
+            Console.ReadKey();
+        }
+    }
+}
